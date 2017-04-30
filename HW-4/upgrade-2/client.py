16a0c0c051c1429a026f4822faa662a23db2b443
st424204 <st424204@yahoo.com.tw>
2017-04-30 17:11:08 +0800

    add redlock

--- HW-4/upgrade-2/client.py
+++ HW-4/upgrade-2/client.py
@@ -8,28 +8,29 @@ import re
 r = re.compile("([a-zA-Z]*)([0-9]*)")
 
 def cmp(x):
-        a = r.match(x[0])
-        try:
-                b = a.group(1)
-        except IndexError:
-                b = ""
-        try:
-                c = a.group(2)
-        except IndexError:
-                c = "0" 
-        if c=='':
-                c="0"
-        return (b,int(c))
+	a = r.match(x[0])
+	try:
+		b = a.group(1)
+	except IndexError:
+		b = ""
+	try:
+		c = a.group(2)
+	except IndexError:
+		c = "0" 
+	if c == '':
+		c = "0"
+	return (b,int(c))
 
-sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(3)]
+
+sockets = list(3)
 for i in range(3):
+	sockets[i] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sockets[i].connect(('node%d'%(i+1),8888))
 	sockets[i].setblocking(0)
 
 def sendcommand(messages,sockets,delay):
 	for i in range(len(messages)):
 		time.sleep(delay)
-#		print messages[i]
 		sockets[i%3].sendall(messages[i])
 
 
@@ -40,25 +41,23 @@ threading.Thread(target=sendcommand,args=(messages,sockets,.01)).start()
 
 total = 0
 succ = 0
-count = 0
 output = ""
-while output=="":
+while output == "" :
 	readable, writable, exceptional = select.select(sockets,[], [])
 	for s in readable:
 		data = s.recv(1024)
-#		print data
-		if data:
-			try:
-				output = json.loads(data)
-			except ValueError, e:
-				output = ""
-			if output == "":
-				if data != 'error':
-					succ = succ + 1
-				total = total + 1
-output = sorted(output.items(), key=cmp,reverse=False)
-for a,b in output:
-	print '%s : %s'%(a,b)
-print '\nsuccess rate : (%d/%d)'%(succ,total)
+		if data == 'end' :
+			data = s.recv(1024)
+			data = s.recv(int(dat))
+			output = json.loads(data)
+		else: 
+			if data != 'error' :
+				succ = succ + 1
+			total = total + 1
+
 for i in range(3):
 	sockets[i].close()
+output = sorted(output.items(), key=cmp,reverse=False)
+for account,money in output:
+	print '%s : %s'%(account,money)
+print '\nsuccess rate : (%d/%d)'%(succ,total)
--- HW-4/upgrade-2/server.py
+++ HW-4/upgrade-2/server.py
@@ -1,89 +1,79 @@
-import socket
-import sys
-import redis
-import json
-
+import socket,sys,redis,json,select
+from redlock import Redlock
 
 def money_check(s):
 	try:
-    		n = int(s)
+		n = int(s)
 	except ValueError:
-    		return -1
+		return -1
 	else:
 		return n
 
-
 r = redis.Redis(host='node1',port=6379,db=0)
+dlm = Redlock([{"host": "node1", "port": 6379, "db": 0}, ])
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 server_address = ('node1',8888)
-
-#print >>sys.stderr,'starting up on %s port %s'%server_address
 sock.bind(server_address)
 sock.listen(1)
 
 while 1:
-#	print >> sys.stderr,'waiting for a connection'
 	connection , client_address = sock.accept()
 	try:
-#		print >> sys.stderr,'connection from',client_address
 		while 1:
-			data = connection.recv(1024000)
-			print data
+			data = connection.recv(1024)
 			if data == 'end':
-				connection.sendall(json.dumps(r.hgetall('mybank')))
-#				print >> sys.stderr,'no more data from',client_address
+				keys = r.keys('*')
+				data = dict()
+				for key in keys:
+					data[key] = r.get(key)
+				data = json.dumps(data);
+				connection.sendall('end')
+				connection.sendall(str(len(data)))
+				connection.sendall(data)
 				break
 			elif data:
-#				print >> sys.stderr,'received "%s"'%data
 				args = data.split()
+				data = "error"
 				if args[0]=='init':
 					account = args[1].lower()
 					money = money_check(args[2])
-					data = "error"
-					succ = 0
-					if money >= 0 and r.hexists('mybank',account) == False :
-						r.hset('mybank',account,money)
-						succ = 1
-						data = "%s,%s,%d"%(account,r.hget('mybank',account),succ)
+					if money >= 0 and r.exists(account) == False :
+						account_lock = dlm.lock(account,1000)
+						r.set(account,money)
+						dlm.unlock(account_lock)
+						data = "ok"
 				elif args[0]=='save':
 					account = args[1].lower()
 					money = money_check(args[2])
-					data = "error"
-                                        succ = 0
-                                        if money >= 0 and r.hexists('mybank',account):
-                                        	r.hincrby('mybank',account,money)
-                                                succ = 1
-                                                data = "%s,%s,%d"%(account,r.hget('mybank',account),succ)
-
+					if money >= 0 and r.exists(account):
+						account_lock = dlm.lock(account,1000)
+						r.incr(account,money)
+						dlm.unlock(account_lock)
+						data = "ok"
 				elif args[0]=='load':
 					account = args[1].lower()
 					money = money_check(args[2])
-					data = "error"
-                                        succ = 0
-					if money >= 0 and r.hexists('mybank',account) and int(r.hget('mybank',account)) >= money :
-                                        	r.hincrby('mybank',account,money*-1)
-                                                succ = 1
-                                                data = "%s,%s,%d"%(account,r.hget('mybank',account),succ)
+					if money >= 0 and r.exists(account) and int(r.get(account)) >= money :
+						account_lock = dlm.lock(account,1000)
+                        r.decr(account,money)
+						dlm.unlock(account_lock)
+                        data = "ok"
 				elif args[0]=='remit':
 					account_A = args[1].lower()
 					account_B = args[2].lower()
 					money = money_check(args[3])
-					data = "error"
-                                        succ = 0
-					if money >= 0 and r.hexists('mybank',account_A) and r.hexists('mybank',account_B) and int(r.hget('mybank',account_A)) >= money and account_A!=account_B:
+					if money >= 0 and r.exists(account_A) and r.exists(account_B) and int(r.get(account_A)) >= money and account_A!=account_B:
 						succ = 1
-						r.hincrby('mybank',account_A,money*-1)
-						r.hincrby('mybank',account_B,money)
-						data = "%s,%s"%(account_A,r.hget('mybank',account_A))
-						data += ",%s,%s,%d"%(account_B,r.hget('mybank',account_B),succ)
-				else:
-					data = "error"
-#				print >> sys.stderr,'sending "%s" back to the client'%(data)
+						account_A_lock = dlm.lock(account_A,1000)
+						account_B_lock = dlm.lock(account_B,1000)
+						r.decr(account_A,money)
+						r.incr(account_B,money)
+						dlm.unlock(account_A_lock)
+						dlm.unlock(account_B_lock)
+						data = "ok"
 				connection.sendall(data)
 			else:
-#				print >> sys.stderr,'no more data from',client_address
 				break;
 	finally:
 		connection.close()
